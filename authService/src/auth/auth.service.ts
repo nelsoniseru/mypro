5c2eb42af3412d527688b295cb6f39d06f6f31e7
nelson <iseru.nelson@voyatekgroup.com>
2024-08-22 21:11:15 +0100

    created controller service  my authservice

--- authService/src/auth/auth.service.ts
+++ authService/src/auth/auth.service.ts
@@ -1,10 +1,7 @@
 import { Injectable,HttpException, HttpStatus,BadRequestException,Inject } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom} from 'rxjs';
-import * as bcrypt from 'bcrypt';
 
 
 @Injectable()
@@ -14,7 +11,7 @@ export class AuthService {
     @Inject('ORDER_SERVICE')  private rabbitClient: ClientProxy
   ) {}
   async login() {
-      const payload = { id:1, sub:"nel@gmail.com" };
+      const payload = { id:1, sub:"e@gmail.com" };
       return {
         statusCode: 200,
         access_token: this.jwtService.sign(payload),
